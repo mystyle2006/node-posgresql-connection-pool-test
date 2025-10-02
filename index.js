ba2ee03069dd6c5bbc731ef9ae3fcf2d54c3029f
inno <sayyou0918@gmail.com>
2025-10-03 00:20:39 +0900

    Init

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const pg = require('pg');
+
+const app = express()
+const port = 3000
+
+const client = new pg.Pool({
+    host: 'localhost',
+    user: 'test',
+    password: 'test',
+    database: 'test',
+    port: 5432,
+    max: 5,
+})
+
+client.connect(err => {
+    if (err) {
+        console.log('Failed to connect db ' + err)
+    } else {
+        console.log('Connect to db done!')
+    }
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/test-timeout', async (req, res) => {
+    const start = new Date();
+    try {
+        await client.query('SELECT pg_sleep(3);');
+        const lag = new Date() - start;
+        console.log(`Lag: \t${lag} ms`);
+    } catch (e) {
+        const lag = new Date() - start;
+        console.log(`Lag: \t${lag} ms`);
+        console.error('pg error', e);
+    }
+
+    res.send('test-timeout!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
